4b49fe0626a7cd01e2e2d89171e61e9b4a3080e9
Zwi <anisha.matanguihan@student.ateneo.edu>
2025-02-21 15:19:31 +0800

    Update models.py

--- M3E-Models-LEDA_MATANGUIHAN_TAN/MyInventorySystem/MyInventoryApp/models.py
+++ M3E-Models-LEDA_MATANGUIHAN_TAN/MyInventorySystem/MyInventoryApp/models.py
@@ -11,3 +11,17 @@ class Supplier(models.Model):
     
     def __str__(self):
         return self.name + " - " + self.city + ", " + self.country + " created at: " + str(self.created_at)
+
+class WaterBottle(models.Model):
+    sku = models.CharField(max_length=12, unique=True) #stock keeping unit usually have 8-12 digits
+    brand = models.CharField(max_length=300)
+    Cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.CharField(max_length=100)
+    mouth_size = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    supplied_by = models.ForeignKey(Supplier,on_delete=models.SET_NULL, null=True, blank=True)
+    current_quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"SKU: {self.sku}: {self.brand}, {self.mouth_size}, {self.size}, {self.color}, {self.supplied_by}, {self.cost}: {self.current_quantity}"
+    
